3e1694a3596652834e25042e39094fd718abd37a
Sadrakh Situmeang <sadrakhsitumeang2004@gmail.com>
2024-10-24 12:48:45 +0700

    slide two

--- lib/views/screens/account_summary/pages/slideTwo/slideTwo.dart
+++ lib/views/screens/account_summary/pages/slideTwo/slideTwo.dart
@@ -1,10 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:icon_sumarry_app/views/screens/account_summary/widget/slideTwo/populerEmailWidget.dart';
+import 'package:icon_sumarry_app/views/screens/account_summary/widget/slideTwo/populerEngagementWidget%20.dart';
+import 'package:icon_sumarry_app/views/screens/account_summary/widget/slideTwo/populerPhoneNumberWidget.dart';
+import 'package:icon_sumarry_app/views/screens/account_summary/widget/slideTwo/populerWordsWidget.dart';
 
 class SlideTwo extends StatelessWidget {
   const SlideTwo({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final screenWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: SingleChildScrollView(
+        // Padding dari tepi screen
+        padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.08),
+        child: Column(
+          children: [
+            SizedBox(height: 15),
+            PopularWordsWidget(),
+            SizedBox(height: 30),
+            PopularEmailWidget(),
+            SizedBox(height: 30),
+            PopularPhoneNumberWidget(),
+            SizedBox(height: 30),
+            PopularEngagementWidget(),
+            SizedBox(height: 30),
+          ],
+        ),
+      ),
+    );
   }
 }
